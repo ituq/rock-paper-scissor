0b372d42c08f01fd391a6be7bb6ae7be1e0766f1
ituq <vincentluisfranz@gmail.com>
2023-09-15 00:24:13 +0200

    added game logic

---
+++ script.js
@@ -0,0 +1,77 @@
+game();
+/* 0=rock, 1=paper, 2=scissor*/
+function getComputerChoice(){
+    let choice = 2* Math.random();
+    return Math.round(choice);
+}
+function decide(playerChoice, computerChoice){
+    if(playerChoice==1){
+        if(computerChoice==0){
+            return "Win! paper beats rock"
+        }
+        else if(computerChoice==1){
+            return "Draw!"
+        }
+        else{
+            return"Loss! Scissor beats rock"
+        }
+    }
+    else if(playerChoice==0){
+        if(playerChoice==computerChoice){
+            return "Draw!"
+        }
+        else if(computerChoice==1){
+            return "Loss! Paper beats rock"
+        }
+        else{
+            return "Win! Rock beats scissor"
+        }
+    }
+    else if(playerChoice==2){
+        if(playerChoice==computerChoice){
+            return "Draw!"
+        }
+        else if(computerChoice==1){
+            return "Win! Scissors beat paper"
+        }
+        else{
+            return "Loss! Rocks beats Scissor"
+        }
+    }
+    else{
+        console.warn("Player made wrong input");
+    }
+}
+function convert(input){
+    input=input.toLowerCase();
+    switch (input) {
+        case "rock":
+            return 0;
+        case "paper":
+            return 1;
+        case "scissor":
+            return 2;
+        default:
+            return 3;
+    }
+}
+function game(){
+    let scorePlayer=0,scoreMachine =0;
+    for(let i =0;i<5;i++){
+        let input=convert(prompt("What is your move?"));
+        let machine=getComputerChoice();
+        let output=decide(input,machine);
+        if(output.charAt(0)=="W"){
+            scorePlayer++;
+        }
+        else if(output.charAt(0)=="L"){
+            scoreMachine++;
+        }
+        else{
+            /*retry round*/
+            i--;
+        }
+        console.log(output);
+    }
+    console.log(scorePlayer>scoreMachine ? `You won by ${scorePlayer-scoreMachine} Points!`: `You lost by ${scoreMachine-scorePlayer} points`)
+}
